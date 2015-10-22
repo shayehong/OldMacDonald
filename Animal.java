eb0c5bfdb43dc1a49ad19f3343ea6471babae4a8
shayehong <shhong1@697-334-Bywater.sf.k12.internal>
2015-10-22 11:35:25 -0700

    Did the first 2 steps

--- Animal.java
+++ Animal.java
@@ -1,4 +1,15 @@
 interface Animal 
 {    
 	   //your code here
-}  
+	public String getSound();
+	public String getType();  
+
+public void setup() {     
+    Cow c = new Cow("cow", "moo");   
+    Chick ch = new Chick("chick","chirp");
+    Pig p = new Pig("pig","oink")
+    System.out.println(c.getType() + " goes " + c.getSound());   
+    System.out.println(ch.getType() + " goes " + ch.getSound()); 
+    System.out.println(p.getType() + " goes " + p.getSound()); 
+	   }
+}
